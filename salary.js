bd81d95a226b3a49be76fd70f93ee8d228ba3619
belammuia <belammuia0@gmail.com>
2023-03-28 10:05:34 +0300

    update

--- salary.js
+++ salary.js
@@ -1,12 +1,14 @@
-             // define payee rates
-// Define tax rates and contribution percentages
-const PAYEE_RATE = [0.1,0.25,0.3]; // 10%,25%,30% payee tax
-const NHIF_RATE = 0.25; // 25% NHIF contribution
-const NSSF_RATE = 0.01; // 1% NSSF contribution
+function calculateSalary(){
+// define payee rates
+
+             // Define tax rates and contribution percentages
+const PAYEE_RATE = 0.3; // ,30% payee tax
+const NHIF_RATE = 600; // 600 NHIF contribution
+const NSSF_RATE = 300; // 300 NSSF contribution
 
 // Prompt user for input of basic salary and benefits
-const basicSalary = Number(prompt("Enter your basic salary:"));
-const benefits = Number(prompt("Enter your benefits:"));
+const basicSalary = parseFloat(document.getElementById('i').value);
+const benefits = parseFloat(document.getElementById('z').value);
 
 // Calculate gross salary by adding basic salary and benefits
 const grossSalary = basicSalary + benefits;
@@ -25,3 +27,9 @@ const netSalary = grossSalary - payee - nhifDeduction - nssfDeduction;
 
 
 
+document.getElementById('f').innerHTML = `kes ${grossSalary.toFixed(1)}`;
+document.getElementById('d').innerHTML = `kes ${nhifDeduction.toFixed(1)}`;
+document.getElementById('e').innerHTML = `kes ${nssfDeduction.toFixed(1)}`;
+document.getElementById('c').innerHTML = `kes ${payee.toFixed(1)}`;
+document.getElementById('g').innerHTML = `kes ${netSalary.toFixed(1)}`;
+}
